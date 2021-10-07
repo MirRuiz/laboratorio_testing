a0eff8830ea1fc30675cc6fd87c6b6867d51804c
MirRuiz <miriamruizruiz1982@gmail.com>
2021-10-07 11:02:34 +0200

    First commit

---
+++ config/test/jest.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rootDir: '../../',
+  preset: 'ts-jest',
+  restoreMocks: true,
+  moduleDirectories: ['<rootDir>/src', 'node_modules'],
+  setupFilesAfterEnv: ['<rootDir>/config/test/setup.ts'],
+  moduleDirectories: ['<rootDir>/src', 'node_modules'],
+};
